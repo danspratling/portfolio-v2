cd27393fbf5f68412208bd5af2744abc2dd1657e
Dan Spratling <dan@skyward.digital>
2025-01-24 10:11:30 +0000

    Sets dark badge variants as defaults

--- src/components/Badge/Badge.tsx
+++ src/components/Badge/Badge.tsx
@@ -75,42 +75,49 @@ export const Badge = ({
           "[&>svg]:-mx-0.5 [&>svg]:size-5 [&>*>svg]:-mx-0.5 [&>*>svg]:size-5",
 
         // colors
-        selectedColor === "gray" &&
-          "bg-gray-50 text-gray-700 border-gray-200 dark:bg-gray-950 dark:text-gray-300 dark:border-gray-800",
-        selectedColor === "red" &&
-          "bg-red-50 text-red-700 border-red-200 dark:bg-red-950 dark:text-red-300 dark:border-red-800",
+        selectedColor === "gray" && "bg-gray-950 text-gray-300 border-gray-800", //"bg-gray-50 text-gray-700 border-gray-200 dark:bg-gray-950 dark:text-gray-300 dark:border-gray-800",
+        selectedColor === "red" && "bg-red-950 text-red-300 border-red-800", //"bg-red-50 text-red-700 border-red-200 dark:bg-red-950 dark:text-red-300 dark:border-red-800",
         selectedColor === "orange" &&
-          "bg-orange-50 text-orange-700 border-orange-200 dark:bg-orange-950 dark:text-orange-300 dark:border-orange-800",
+          "bg-orange-950 text-orange-300 border-orange-800",
+        //"bg-orange-50 text-orange-700 border-orange-200 dark:bg-orange-950 dark:text-orange-300 dark:border-orange-800",
         selectedColor === "amber" &&
-          "bg-amber-50 text-amber-700 border-amber-200 dark:bg-amber-950 dark:text-amber-300 dark:border-amber-800",
+          "bg-amber-950 text-amber-300 border-amber-800",
+        // "bg-amber-50 text-amber-700 border-amber-200 dark:bg-amber-950 dark:text-amber-300 dark:border-amber-800",
         selectedColor === "yellow" &&
-          "bg-yellow-50 text-yellow-700 border-yellow-200 dark:bg-yellow-950 dark:text-yellow-300 dark:border-yellow-800",
-        selectedColor === "lime" &&
-          "bg-lime-50 text-lime-700 border-lime-200 dark:bg-lime-950 dark:text-lime-300 dark:border-lime-800",
+          "bg-yellow-950 text-yellow-300 border-yellow-800",
+        // "bg-yellow-50 text-yellow-700 border-yellow-200 dark:bg-yellow-950 dark:text-yellow-300 dark:border-yellow-800",
+        selectedColor === "lime" && "bg-lime-950 text-lime-300 border-lime-800",
+        // "bg-lime-50 text-lime-700 border-lime-200 dark:bg-lime-950 dark:text-lime-300 dark:border-lime-800",
         selectedColor === "green" &&
-          "bg-green-50 text-green-700 border-green-200 dark:bg-green-950 dark:text-green-300 dark:border-green-800",
+          "bg-green-950 text-green-300 border-green-800",
+        // "bg-green-50 text-green-700 border-green-200 dark:bg-green-950 dark:text-green-300 dark:border-green-800",
         selectedColor === "emerald" &&
-          "bg-emerald-50 text-emerald-700 border-emerald-200 dark:bg-emerald-950 dark:text-emerald-300 dark:border-emerald-800",
-        selectedColor === "teal" &&
-          "bg-teal-50 text-teal-700 border-teal-200 dark:bg-teal-950 dark:text-teal-300 dark:border-teal-800",
-        selectedColor === "cyan" &&
-          "bg-cyan-50 text-cyan-700 border-cyan-200 dark:bg-cyan-950 dark:text-cyan-300 dark:border-cyan-800",
-        selectedColor === "sky" &&
-          "bg-sky-50 text-sky-700 border-sky-200 dark:bg-sky-950 dark:text-sky-300 dark:border-sky-800",
-        selectedColor === "blue" &&
-          "bg-blue-50 text-blue-700 border-blue-200 dark:bg-blue-950 dark:text-blue-300 dark:border-blue-800",
+          "bg-emerald-950 text-emerald-300 border-emerald-800",
+        // "bg-emerald-50 text-emerald-700 border-emerald-200 dark:bg-emerald-950 dark:text-emerald-300 dark:border-emerald-800",
+        selectedColor === "teal" && "bg-teal-950 text-teal-300 border-teal-800",
+        // "bg-teal-50 text-teal-700 border-teal-200 dark:bg-teal-950 dark:text-teal-300 dark:border-teal-800",
+        selectedColor === "cyan" && "bg-cyan-950 text-cyan-300 border-cyan-800",
+        // "bg-cyan-50 text-cyan-700 border-cyan-200 dark:bg-cyan-950 dark:text-cyan-300 dark:border-cyan-800",
+        selectedColor === "sky" && "bg-sky-950 text-sky-300 border-sky-800",
+        // "bg-sky-50 text-sky-700 border-sky-200 dark:bg-sky-950 dark:text-sky-300 dark:border-sky-800",
+        selectedColor === "blue" && "bg-blue-950 text-blue-300 border-blue-800",
+        // "bg-blue-50 text-blue-700 border-blue-200 dark:bg-blue-950 dark:text-blue-300 dark:border-blue-800",
         selectedColor === "indigo" &&
-          "bg-indigo-50 text-indigo-700 border-indigo-200 dark:bg-indigo-950 dark:text-indigo-300 dark:border-indigo-800",
+          "bg-indigo-950 text-indigo-300 border-indigo-800",
+        // "bg-indigo-50 text-indigo-700 border-indigo-200 dark:bg-indigo-950 dark:text-indigo-300 dark:border-indigo-800",
         selectedColor === "violet" &&
-          "bg-violet-50 text-violet-700 border-violet-200 dark:bg-violet-950 dark:text-violet-300 dark:border-violet-800",
+          "bg-violet-950 text-violet-300 border-violet-800",
+        // "bg-violet-50 text-violet-700 border-violet-200 dark:bg-violet-950 dark:text-violet-300 dark:border-violet-800",
         selectedColor === "purple" &&
-          "bg-purple-50 text-purple-700 border-purple-200 dark:bg-purple-950 dark:text-purple-300 dark:border-purple-800",
+          "bg-purple-950 text-purple-300 border-purple-800",
+        // "bg-purple-50 text-purple-700 border-purple-200 dark:bg-purple-950 dark:text-purple-300 dark:border-purple-800",
         selectedColor === "fuchsia" &&
-          "bg-fuchsia-50 text-fuchsia-700 border-fuchsia-200 dark:bg-fuchsia-950 dark:text-fuchsia-300 dark:border-fuchsia-800",
-        selectedColor === "pink" &&
-          "bg-pink-50 text-pink-700 border-pink-200 dark:bg-pink-950 dark:text-pink-300 dark:border-pink-800",
-        selectedColor === "rose" &&
-          "bg-rose-50 text-rose-700 border-rose-200 dark:bg-rose-950 dark:text-rose-300 dark:border-rose-800",
+          "bg-fuchsia-950 text-fuchsia-300 border-fuchsia-800",
+        // "bg-fuchsia-50 text-fuchsia-700 border-fuchsia-200 dark:bg-fuchsia-950 dark:text-fuchsia-300 dark:border-fuchsia-800",
+        selectedColor === "pink" && "bg-pink-950 text-pink-300 border-pink-800",
+        // "bg-pink-50 text-pink-700 border-pink-200 dark:bg-pink-950 dark:text-pink-300 dark:border-pink-800",
+        selectedColor === "rose" && "bg-rose-950 text-rose-300 border-rose-800",
+        // "bg-rose-50 text-rose-700 border-rose-200 dark:bg-rose-950 dark:text-rose-300 dark:border-rose-800",
 
         // transparent
         transparent && "bg-transparent shadow-sm",
