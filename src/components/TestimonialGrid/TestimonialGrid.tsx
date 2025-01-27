81b6135d9f7aaa59ed63e7d117988c9703324f1c
Dan Spratling <dan@skyward.digital>
2025-01-27 16:34:48 +0000

    Improves the balance of testimonial grid

--- src/components/TestimonialGrid/TestimonialGrid.tsx
+++ src/components/TestimonialGrid/TestimonialGrid.tsx
@@ -14,6 +14,29 @@ export const TestimonialGrid = ({
   link,
   testimonials,
 }: TestimonialGridProps) => {
+  const sortedTestimonials = (() => {
+    const arr: TestimonialProps[][] = [[], [], []]
+
+    testimonials
+      .filter((testimonial) => testimonial.testimonial)
+      .sort((a, b) => a.testimonial.length - b.testimonial.length)
+      .map((testimonial, index) => {
+        if (index % 3 === 0) arr[1].push(testimonial)
+        else if (index % 3 === 1) arr[0].push(testimonial)
+        else arr[2].push(testimonial)
+      })
+
+    const shuffleArray = (array) =>
+      array
+        .map((value) => ({ value, sort: Math.random() }))
+        .sort((a, b) => a.sort - b.sort)
+        .map(({ value }) => value)
+
+    return [shuffleArray(arr[0]), shuffleArray(arr[1]), shuffleArray(arr[2])]
+  })()
+
+  console.log(sortedTestimonials)
+
   return (
     <section className="bg-gray-950 text-gray-200 py-12">
       <div className="container mx-auto my-20 xl:my-40 grid gap-6">
@@ -31,40 +54,19 @@ export const TestimonialGrid = ({
 
         <div className="grid gap-3 md:grid-cols-3 my-4">
           <div className="flex flex-col gap-3">
-            {testimonials
-              .slice(0, Math.ceil(testimonials.length / 3))
-              .map((testimonial) => (
-                <TestimonialCard
-                  key={testimonial.author.name}
-                  {...testimonial}
-                />
-              ))}
+            {sortedTestimonials[0].map((testimonial) => (
+              <TestimonialCard key={testimonial.author.name} {...testimonial} />
+            ))}
           </div>
           <div className="flex flex-col gap-3">
-            {testimonials
-              .slice(
-                Math.ceil(testimonials.length / 3),
-                Math.ceil(testimonials.length / 3) * 2
-              )
-              .map((testimonial) => (
-                <TestimonialCard
-                  key={testimonial.author.name}
-                  {...testimonial}
-                />
-              ))}
+            {sortedTestimonials[1].map((testimonial) => (
+              <TestimonialCard key={testimonial.author.name} {...testimonial} />
+            ))}
           </div>
           <div className="flex flex-col gap-3">
-            {testimonials
-              .slice(
-                Math.ceil(testimonials.length / 3) * 2,
-                testimonials.length
-              )
-              .map((testimonial) => (
-                <TestimonialCard
-                  key={testimonial.author.name}
-                  {...testimonial}
-                />
-              ))}
+            {sortedTestimonials[2].map((testimonial) => (
+              <TestimonialCard key={testimonial.author.name} {...testimonial} />
+            ))}
           </div>
         </div>
       </div>
